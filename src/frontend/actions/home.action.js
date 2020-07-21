02dd931766cd0eb1c62aad48f1a55803678b664b
Eduardo Alvarez <eduardo.a1993@gmail.com>
2020-07-21 17:05:33 -0400

    Create get home action

---
+++ src/frontend/actions/home.action.js
@@ -0,0 +1,30 @@
+import axios from 'axios';
+import {
+	GET_HOME_DATA_SUCCESS,
+	GET_HOME_DATA_ERROR,
+} from '../config/constants';
+
+export const getHomeDataAction = (dispatch) => async () => {
+	try {
+		const url = 'https://api.eduardoalvarez.cl/node/api/users';
+		const { data } = await axios({
+			url,
+			method: 'GET',
+		});
+		return dispatch({
+			type: GET_HOME_DATA_SUCCESS,
+			payload: {
+				type: 'success',
+				content: data.content,
+			},
+		});
+	} catch (error) {
+		return dispatch({
+			type: GET_HOME_DATA_ERROR,
+			payload: {
+				type: 'error',
+				error_message: error,
+			},
+		});
+	}
+};
--- src/frontend/reducers/home.reducer.js
+++ src/frontend/reducers/home.reducer.js
@@ -1,9 +1,28 @@
+import {
+	GET_HOME_DATA_SUCCESS,
+	GET_HOME_DATA_ERROR,
+} from '../config/constants';
+
 const initialState = {
-	message: 'Hello',
+	type: '',
+	content: {},
+	error_message: '',
 };
 
 export default (state = initialState, action) => {
 	switch (action.type) {
+		case GET_HOME_DATA_SUCCESS:
+			return {
+				...state,
+				type: action.payload.type,
+				content: action.payload.content,
+			};
+		case GET_HOME_DATA_ERROR:
+			return {
+				...state,
+				type: action.payload.type,
+				error_message: action.payload.error_message,
+			};
 		default:
 			return state;
 	}

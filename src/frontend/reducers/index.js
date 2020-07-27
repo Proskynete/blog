7cf85e2f4ead263fe94a4407d10ec51d469dd745
Eduardo Alvarez <eduardo.a1993@gmail.com>
2020-07-26 21:16:16 -0400

    About me view

--- src/frontend/reducers/index.js
+++ src/frontend/reducers/index.js
@@ -1,2 +1,3 @@
 export { default as homeReducer } from './home.reducer';
 export { default as articleReducer } from './article.reducer';
+export { default as aboutMeReducer } from './about-me.reducer';
--- src/frontend/store.js
+++ src/frontend/store.js
@@ -1,7 +1,7 @@
 import window from 'global';
 import { createStore, combineReducers, compose, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
-import { homeReducer, articleReducer } from './reducers';
+import { homeReducer, articleReducer, aboutMeReducer } from './reducers';
 
 const preloadedState = window.__PRELOADED_STATE__;
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
@@ -9,6 +9,7 @@ const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const rootReducer = combineReducers({
 	home: homeReducer,
 	article: articleReducer,
+	aboutMe: aboutMeReducer,
 });
 
 const store = createStore(
